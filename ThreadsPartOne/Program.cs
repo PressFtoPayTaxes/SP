ed973d9c3f0900d8674938db7608827d8bfc1012
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-05-30 17:43:50 +0600

    Init

---
+++ ThreadsPartOne/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Media;
+using System.IO;
+
+namespace ThreadsPartOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread musicThread = new Thread(PlayMusic);
+            musicThread.IsBackground = true;
+            musicThread.Start("shrek_09. Smash Mouth - All Star.wav");
+
+            Console.WriteLine("Enter your text here:");
+
+            var text = new TextForSaving();
+            text.FilePath = "text.txt";
+            text.Text = Console.ReadLine();
+
+            Thread savingThread = new Thread(SavingInFile);
+            savingThread.Start(text);
+        }
+
+        private static void PlayMusic(object audioFile)
+        {
+            var audio = audioFile as string;
+            var player = new SoundPlayer(audio);
+            player.PlayLooping();
+        }
+
+        private static void SavingInFile(object textForSaving)
+        {
+            var text = textForSaving as TextForSaving;
+
+            using (var writer = new StreamWriter(text.FilePath))
+            {
+                writer.WriteLine(text.Text);
+            }
+        }
+    }
+}
---
+++ ThreadsPartOne/TextForSaving.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadsPartOne
+{
+    public class TextForSaving
+    {
+        public string FilePath { get; set; }
+        public string Text { get; set; }
+    }
+}
