2a88ada5b2586b90beddad76d596d8c2bf7cf583
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-06-10 18:03:55 +0600

    Init

---
+++ WithSynchronization/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace WithSynchronization
+{
+    public class BankAccount
+    {
+        public int Money { get; set; }
+
+        public BankAccount()
+        {
+            Money = 50000;
+        }
+
+        private object lockObject = new object();
+        public void MakeOperation()
+        {
+            var currentThread = Thread.CurrentThread;
+            Console.WriteLine($"Поток {currentThread.ManagedThreadId} начал работу");
+
+            lock (lockObject)
+            {
+                int passedSeconds = 0;
+                while (passedSeconds < 60)
+                {
+                    Random random = new Random();
+                    Thread.Sleep(5000);
+                    int randomNumber = random.Next(-10000, 10000);
+                    Money += randomNumber;
+                    Console.WriteLine($"На счет перечислилось {randomNumber}\nТекущий счёт: {Money}");
+                    passedSeconds += 5;
+                }
+            }
+
+            Console.WriteLine($"Поток {currentThread.ManagedThreadId} Завершил работу");
+        }
+    }
+}
---
+++ WithSynchronization/User.cs
@@ -0,0 +1,14 @@
+﻿namespace WithSynchronization
+{
+    public  class User
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public BankAccount BankAccount { get; set; }
+
+        public User()
+        {
+            BankAccount = new BankAccount();
+        }
+    }
+}
