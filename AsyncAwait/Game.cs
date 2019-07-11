ff8c001425ecb2c7b0aa8fdcf62c9c2a3dd1e2d7
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-07-11 20:11:07 +0600

    Init

---
+++ AsyncAwait/Game.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    public class Game
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("alternative_names")]
+        public List<int> AlternativeNames { get; set; }
+        [JsonProperty("category")]
+        public int Category { get; set; }
+        [JsonProperty("collection")]
+        public int Collection { get; set; }
+        [JsonProperty("cover")]
+        public int Cover { get; set; }
+        [JsonProperty("created_at")]
+        public int CreationDate { get; set; }
+        [JsonProperty("external_games")]
+        public List<int> ExternalGames { get; set; }
+        [JsonProperty("first_release_date")]
+        public int FirstReleaseDate { get; set; }
+        [JsonProperty("franchise")]
+        public int Franchise { get; set; }
+        [JsonProperty("franchises")]
+        public List<int> Franchises { get; set; }
+        [JsonProperty("game_modes")]
+        public List<int> GameModes { get; set; }
+        [JsonProperty("genres")]
+        public List<int> Genres { get; set; }
+        [JsonProperty("involved_companies")]
+        public List<int> InvolvedCompanies { get; set; }
+        [JsonProperty("keywords")]
+        public List<int> Keywords { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("platforms")]
+        public List<int> Platforms { get; set; }
+        [JsonProperty("player_perspectives")]
+        public List<int> PlayerPerspectives { get; set; }
+        [JsonProperty("popularity")]
+        public double Popularity { get; set; }
+        [JsonProperty("pulse_count")]
+        public int PulseCount { get; set; }
+        [JsonProperty("release_dates")]
+        public List<int> ReleaseDates { get; set; }
+        [JsonProperty("screeshots")]
+        public List<int> Screenshots { get; set; }
+        [JsonProperty("similar_games")]
+        public List<int> SimilarGames { get; set; }
+        [JsonProperty("slug")]
+        public string Slug { get; set; }
+        [JsonProperty("tags")]
+        public List<int> Tags { get; set; }
+        [JsonProperty("themes")]
+        public List<int> Themes { get; set; }
+        [JsonProperty("updated_at")]
+        public int UpdateDate { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("videos")]
+        public List<int> Videos { get; set; }
+        [JsonProperty("websites")]
+        public List<int> Websites { get; set; }
+    }
+}
---
+++ AsyncAwait/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncAwait
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Game> _games = new List<Game>();
+
+        private async void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            List<Id> idList = new List<Id>();
+
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("user-key", "b54767b80a891fd65001937d4f40d44f");
+
+                string jsonString = await Task.Run(() => client.DownloadString("https://api-v3.igdb.com/games/?platform=pc"));
+                idList = JsonConvert.DeserializeObject<List<Id>>(jsonString);
+
+
+                jsonString = await Task.Run(() => client.DownloadString($"https://api-v3.igdb.com/games/?id={idList[0].Identifier}&fields=*"));
+                _games.AddRange(JsonConvert.DeserializeObject<Game[]>(jsonString));
+            }
+
+            CardsFilling();
+        }
+
+        private async void CardsFilling()
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("user-key", "b54767b80a891fd65001937d4f40d44f");
+
+
+                firstNameTextBox.Text = _games[0].Name;
+                //string jsonString = await Task.Run(() => client.DownloadString("https://api-v3.igdb.com/genres/{_games[0].Genres[0]}/?fields=*"));
+                firstReleaseDateTextBox.Text = "Дата релиза: " + string.Concat(new DateTime((_games[0].FirstReleaseDate)));
+                firstPopularityTextBox.Text = "Популярность: " + string.Concat(_games[0].Popularity);
+
+                secondNameTextBox.Text = _games[1].Name;
+                secondReleaseDateTextBox.Text = "Дата релиза: " + string.Concat(new DateTime((_games[1].FirstReleaseDate)));
+                secondPopularityTextBox.Text = "Популярность: " + string.Concat(_games[1].Popularity);
+
+                thirdNameTextBox.Text = _games[2].Name;
+                thirdReleaseDateTextBox.Text = "Дата релиза: " + string.Concat(new DateTime((_games[2].FirstReleaseDate)));
+                thirdPopularityTextBox.Text = "Популярность: " + string.Concat(_games[2].Popularity);
+
+                forthNameTextBox.Text = _games[3].Name;
+                forthReleaseDateTextBox.Text = "Дата релиза: " + string.Concat(new DateTime((_games[3].FirstReleaseDate)));
+                forthPopularityTextBox.Text = "Популярность: " + string.Concat(_games[3].Popularity);
+
+                fifthNameTextBox.Text = _games[4].Name;
+                fifthReleaseDateTextBox.Text = "Дата релиза: " + string.Concat(new DateTime((_games[4].FirstReleaseDate)));
+                fifthPopularityTextBox.Text = "Популярность: " + string.Concat(_games[4].Popularity);
+
+                sixthNameTextBox.Text = _games[5].Name;
+                sixthReleaseDateTextBox.Text = "Дата релиза: " + string.Concat(new DateTime((_games[5].FirstReleaseDate)));
+                sixthPopularityTextBox.Text = "Популярность: " + string.Concat(_games[5].Popularity);
+
+                seventhNameTextBox.Text = _games[6].Name;
+                seventhReleaseDateTextBox.Text = "Дата релиза: " + string.Concat(new DateTime((_games[6].FirstReleaseDate)));
+                seventhPopularityTextBox.Text = "Популярность: " + string.Concat(_games[6].Popularity);
+
+                eighthNameTextBox.Text = _games[7].Name;
+                eighthReleaseDateTextBox.Text = "Дата релиза: " + string.Concat(new DateTime((_games[7].FirstReleaseDate)));
+                eighthPopularityTextBox.Text = "Популярность: " + string.Concat(_games[7].Popularity);
+
+                ninthNameTextBox.Text = _games[8].Name;
+                ninthReleaseDateTextBox.Text = "Дата релиза: " + string.Concat(new DateTime((_games[8].FirstReleaseDate)));
+                ninthPopularityTextBox.Text = "Популярность: " + string.Concat(_games[8].Popularity);
+
+                tenthNameTextBox.Text = _games[8].Name;
+                tenthReleaseDateTextBox.Text = "Дата релиза: " + string.Concat(new DateTime((_games[9].FirstReleaseDate)));
+                tenthPopularityTextBox.Text = "Популярность: " + string.Concat(_games[9].Popularity);
+            }
+
+        }
+    }
+}
